377c3d1696f9faa29bde1347a5fea09c0fe5a603
GMNDS <gabriel@gmnds.com>
2025-05-02 15:46:59 -0300

    feat: adiciona cálculo automático de end_date no método before_save do Appointment

---
+++ scheduling_system/scheduling_system/doctype/appointment/appointment.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2025, Gabriel Menezes and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from datetime import datetime, timedelta
+
+
+class Appointment(Document):
+	def calculate_end_date(self):
+		if self.start_date and self.duration:
+			hour, minute, second = map(int, self.duration.split(":"))
+			return datetime.strptime(self.start_date, "%Y-%m-%d %H:%M:%S") + timedelta(hours=hour, minutes=minute, seconds=second)
+		return None
+
+	def before_save(self):
+		self.end_date = self.calculate_end_date()
+	
