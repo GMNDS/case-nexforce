95bf9789432b0833198617caf36675d8366df9be
GMNDS <gabriel@gmnds.com>
2025-05-02 15:49:29 -0300

    feat: adiciona validação de conflito de agendamentos por vendedor no método validate

--- scheduling_system/scheduling_system/doctype/appointment/appointment.py
+++ scheduling_system/scheduling_system/doctype/appointment/appointment.py
@@ -16,3 +16,24 @@ class Appointment(Document):
 	def before_save(self):
 		self.end_date = self.calculate_end_date()
 	
+
+	def validate(self):
+		end_date = self.calculate_end_date()
+		confliting_appointments = frappe.db.get_all(
+			"Appointment",
+			filters={
+				"seller": self.seller,
+				"name": ["!=", self.name],
+				"start_date": ["<=", end_date],
+				"end_date": [">=", self.start_date],
+				"status": ["!=", "Canceled"]
+			},
+			fields=["name", "client_name", "start_date", "end_date"]
+		)
+		frappe.log(f"{confliting_appointments}")
+		if confliting_appointments:
+			frappe.throw(
+				f"Scheduling conflict for seller {self.seller}. "
+				f"Already booked with client {confliting_appointments[0].client_name} from {confliting_appointments[0].start_date} to {confliting_appointments[0].end_date}."
+			)	 
+
