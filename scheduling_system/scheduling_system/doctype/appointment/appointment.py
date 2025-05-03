4db8cb619db7cd0996738921fccfdf886c74c455
GMNDS <gabriel@gmnds.com>
2025-05-03 11:46:37 -0300

    fix: corrige `self.end` e `self.start`

--- scheduling_system/scheduling_system/doctype/appointment/appointment.py
+++ scheduling_system/scheduling_system/doctype/appointment/appointment.py
@@ -8,32 +8,32 @@ from datetime import datetime, timedelta
 
 class Appointment(Document):
 	def calculate_end_date(self):
-		if self.start_date and self.duration:
+		if self.start and self.duration:
 			hour, minute, second = map(int, self.duration.split(":"))
-			return datetime.strptime(self.start_date, "%Y-%m-%d %H:%M:%S") + timedelta(hours=hour, minutes=minute, seconds=second)
+			return datetime.strptime(self.start, "%Y-%m-%d %H:%M:%S") + timedelta(hours=hour, minutes=minute, seconds=second)
 		return None
 
 	def before_save(self):
-		self.end_date = self.calculate_end_date()
+		self.end = self.calculate_end_date()
 	
 
 	def validate(self):
-		end_date = self.calculate_end_date()
+		end = self.calculate_end_date()
 		confliting_appointments = frappe.db.get_all(
 			"Appointment",
 			filters={
 				"seller": self.seller,
 				"name": ["!=", self.name],
-				"start_date": ["<=", end_date],
-				"end_date": [">=", self.start_date],
+				"start": ["<=", end],
+				"end": [">=", self.start],
 				"status": ["!=", "Canceled"]
 			},
-			fields=["name", "client_name", "start_date", "end_date"]
+			fields=["name", "client_name", "start", "end"]
 		)
 		frappe.log(f"{confliting_appointments}")
 		if confliting_appointments:
 			frappe.throw(
 				f"Scheduling conflict for seller {self.seller}. "
-				f"Already booked with client {confliting_appointments[0].client_name} from {confliting_appointments[0].start_date} to {confliting_appointments[0].end_date}."
+				f"Already booked with client {confliting_appointments[0].client_name} from {confliting_appointments[0].start} to {confliting_appointments[0].end}."
 			)	 
 
